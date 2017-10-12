335dc2b3aa0a55adaa71ef77c54a93be828180c5
Ruobo Peng <ruobop@gmail.com>
2017-10-12 19:15:27 +0800

    Let's see what it is

--- access_token_handler.py
+++ access_token_handler.py
@@ -6,9 +6,10 @@ import requests
 import time
 
 def get_access_token():
-    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_' +
+    url = ('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_' +
           'credential&appid=wxddae0de1b0e43c9e&secret=3694ea0bc550f58' +
-          '15547d4e182ab42b3'
+          '15547d4e182ab42b3')
+    print url
     response = requests.get(url)
     data = response.json()
     data['create_time'] = time.time()
