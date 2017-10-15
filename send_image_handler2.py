fb86543ea8cff0217e2075ef645da366fe381b69
Ruobo Peng <ruobop@gmail.com>
2017-10-16 00:02:55 +0800

    include only py files

--- send_image_handler2.py
+++ send_image_handler2.py
@@ -14,12 +14,13 @@ def send_text_msg(text, fromuser, touser):
     }
     """
     raw_data = raw_data % (touser, text)
+    print raw_data
     json_data = json.dumps(raw_data)
     access_token = access_token_handler.get_token()
     url = ('https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' +
            '%s')
     url = url % (access_token)
-    payload = {'some': 'data'}
     headers = {'content-type': 'application/json'}
 
     response = requests.post(url, json_data, headers=headers)
+    print response
