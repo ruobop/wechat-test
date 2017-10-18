af73af76056e6f68557b951bf65b08d7c3678034
Ruobo Peng <ruobop@gmail.com>
2017-10-18 14:35:47 +0800

    start

--- wechat_server_celery.py
+++ wechat_server_celery.py
@@ -1,5 +1,5 @@
 #-*-coding:utf-8 -*-
-from flask import Flask #, request
+from flask import Flask, request
 # from flask import make_response
 # import hashlib
 # import MsgParser
@@ -15,29 +15,29 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 # response at 127.0.0.1/weixin/
-# @app.route('/weixin', methods=['GET', 'POST'])
-# def weixin():
-#     if request.method == 'GET':
-#         if len(request.args) > 3:
-#             temparr = []
-#             token = "thisisatest"
-#             signature = request.args["signature"]
-#             timestamp = request.args["timestamp"]
-#             nonce = request.args["nonce"]
-#             echostr = request.args["echostr"]
-#             temparr.append(token)
-#             temparr.append(timestamp)
-#             temparr.append(nonce)
-#             temparr.sort()
-#             newstr = "".join(temparr)
-#             sha1str = hashlib.sha1(newstr)
-#             temp = sha1str.hexdigest()
-#             if signature == temp:
-#                 return echostr
-#             else:
-#                 return "Authentification failed!"
-#         else:
-#             return "Your request method is: " + request.method
+@app.route('/weixin', methods=['GET', 'POST'])
+def weixin():
+    if request.method == 'GET':
+        if len(request.args) > 3:
+            temparr = []
+            token = "thisisatest"
+            signature = request.args["signature"]
+            timestamp = request.args["timestamp"]
+            nonce = request.args["nonce"]
+            echostr = request.args["echostr"]
+            temparr.append(token)
+            temparr.append(timestamp)
+            temparr.append(nonce)
+            temparr.sort()
+            newstr = "".join(temparr)
+            sha1str = hashlib.sha1(newstr)
+            temp = sha1str.hexdigest()
+            if signature == temp:
+                return echostr
+            else:
+                return "Authentification failed!"
+        else:
+            return "Your request method is: " + request.method
 #     elif request.method == 'POST':  # POST
 #         # print "POST"
 #         oridata = request.data # store incoming data
